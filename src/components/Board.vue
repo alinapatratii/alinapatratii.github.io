aff2cfb6875e25a469aecf3c3f71bacc357517c9
Alina Patratii <alina.patratii10@gmail.com>
2024-01-20 03:17:49 +0100

    Initial commit

---
+++ src/components/Board.vue
@@ -0,0 +1,38 @@
+<script setup>
+	import Column from './Column.vue';
+
+	defineProps({
+		columns: {
+            type: Array,
+			required: true
+		}
+	});
+</script>
+
+<template>
+	<div class="container">
+		<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+		<div id="myModal" class="modal fade" role="dialog">
+			<div class="modal-dialog">
+			<!-- Modal content-->
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal">&times;</button>
+						<h4 class="modal-title">Modal Header</h4>
+					</div>
+					<div class="modal-body">
+						<p>Some text in the modal.</p>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div v-for="column in columns" class="col-12 col-md-6 col-lg-4">
+                <Column :key="column.id" :column="column" />
+			</div>
+		</div>
+	</div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<script setup>
+///////////////////////////////////////////////
+// IDs to be used for the respective DOM elements in the template
+///////////////////////////////////////////////
+
+const BUTTON_TOGGLE_MODAL_ID = 'buttonToggleModal' // for the button that opens the modal
+
+///////////////////////////////////////////////
+// Your implementation below
+///////////////////////////////////////////////
+
+defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    /* MODAL_ID: {
+        type: String,
+        required: true
+    } */
+});
+</script>
+
+<template>
+    <nav class="navbar bg-light">
+        <div class="container-fluid">
+            <!-- Logo and title of the Kanban board -->
+            <span class="navbar-brand mb-0 h1">
+                <img src="../assets/logo.svg" alt="Logo" width="24" height="24" class="d-inline-block align-text-top">
+                {{ title }}
+            </span>
+
+            <!-- TODO: button -->
+            <button type="button" class="btn btn-outline-primary" data-toggle=".modal" data-target="#modalRoot" :id="BUTTON_TOGGLE_MODAL_ID">Create new task</button>
+            <button type="button" class="btn btn-primary" data-toggle="collapse" data-target="#exampleModalLong">
+                Launch demo modal
+            </button>
+            <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            ...
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </nav>
+</template>
